46452f7258989e2a0f69dfc8ccae3817917e7cae
om9070 <om70796790@gmail.com>
2023-10-19 21:12:49 +0530

    poolllll

--- src/Home.js
+++ src/Home.js
@@ -43,14 +43,14 @@ const Home = () => {
   
   //render when diffrent stockId.
   useEffect(() => {
-    var options = {
-      rememberUpgrade:true,
-      transports: ['polling', 'websocket'],
-      secure:true, 
-      rejectUnauthorized: false
-          }
+    // var options = {
+    //   rememberUpgrade:true,
+    //   transports: ['polling', 'websocket'],
+    //   secure:true, 
+    //   rejectUnauthorized: false
+    //       }
     // const socket = io(api); // Replace with your server's URL\
-    var socket = io(api,options);
+    var socket = io(api,{transports: ['polling', 'websocket']});
 
 
     const findCurrentStock = dropDownData.find((e) => e._id == stockId);
